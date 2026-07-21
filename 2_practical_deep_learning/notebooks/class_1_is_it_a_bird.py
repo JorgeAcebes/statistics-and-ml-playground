455de5710ed817bd62f84bd636d3f783fc962ca9
Jorge Acebes <jorgea2004@gmail.com>
2026-07-21 20:43:37 +0200

    Starting class 1: Bird Identification

---
+++ 2_practical_deep_learning/notebooks/class_1_is_it_a_bird.py
@@ -0,0 +1,189 @@
+# %% [markdown]
+# ## Is it a bird?
+# %% [markdown]
+
+# Original code [here](https://www.kaggle.com/code/jhoward/is-it-a-bird-creating-a-model-from-your-own-data).
+
+# %%
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+import seaborn as sns
+import time, json
+import sys
+
+
+from numpy import random
+from scipy.stats import norm
+from fastcore.all import *
+from pathlib import Path
+
+
+from ddgs import DDGS #DuckDuckGo has changed the api so we need to update 
+from fastcore.all import *
+
+print('Imported libraries')
+
+# %%
+
+SCRIPT_DIR = Path(__file__).resolve().parent
+PRACTICAL_DL_DIR = SCRIPT_DIR.parent
+DATA_DIR = PRACTICAL_DL_DIR / 'data'
+
+# %% [markdown]
+# ## Step 1: Download images of birds and non-birds
+
+#%%
+def search_images(keywords, max_images=200): 
+    time.sleep(2)
+    return L(DDGS().images(keywords, max_results=max_images)).itemgot('image')
+
+# %% [markdown]
+# Let's start by searching for a bird photo and seeing what kind of result we get. We'll start by getting URLs from a search:
+
+# %%
+#NB: `search_images` depends on duckduckgo.com, which doesn't always return correct responses.
+#    If you get a JSON error, just try running it again (it may take a couple of tries).
+urls = search_images('bird photos', max_images=1)
+urls[0]
+
+# %% [markdown]
+# ...and then download a URL and take a look at it:
+
+# %%
+from fastdownload import download_url
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+dest = DATA_DIR/'bird.jpg'
+download_url(urls[0], dest, show_progress=False)
+
+from fastai.vision.all import *
+im = Image.open(dest)
+im.to_thumb(256,256)
+
+# %% [markdown]
+# Now let's do the same with "forest photos":
+
+# %%
+download_url(search_images('forest photos', max_images=1)[0], 'forest.jpg', show_progress=False)
+Image.open(DATA_DIR/'forest.jpg').to_thumb(256,256)
+
+# %% [markdown]
+# Our searches seem to be giving reasonable results, so let's grab a few examples of each of "bird" and "forest" photos, and save each group of photos to a different folder:
+
+# %%
+searches = 'forest','bird'
+path = Path(DATA_DIR/'bird_or_not')
+
+for o in searches:
+    dest = (path/o)
+    dest.mkdir(exist_ok=True, parents=True)
+    download_images(dest, urls=search_images(f'{o} photo'))
+    time.sleep(5)
+    resize_images(path/o, max_size=400, max_workers=0, dest=path/o)
+
+# %% 
+
+# Checking if there are the same number of data of each type
+
+bird = Path(DATA_DIR/'bird_or_not'/'bird')
+fore = Path(DATA_DIR/'bird_or_not'/'forest')
+
+tot_bird = len(list(bird.iterdir()))
+tot_fore = len(list(fore.iterdir()))
+
+
+print(f'Birds:  {tot_bird}\nForest: {tot_fore}')
+# %% [markdown]
+# ## Step 2: Train our model
+
+# %% [markdown]
+# Some photos might not download correctly which could cause our model training to fail, so we'll remove them:
+
+# %%
+failed = verify_images(get_image_files(path))
+failed.map(Path.unlink)
+len(failed)
+
+# %% [markdown]
+# To train a model, we'll need `DataLoaders`, which is an object that contains a *training set* (the images used to create a model) and a *validation set* (the images used to check the accuracy of a model -- not used during training). In `fastai` we can create that easily using a `DataBlock`, and view sample images from it:
+
+# %%
+dls = DataBlock(
+    blocks=(ImageBlock, CategoryBlock), 
+    get_items=get_image_files, 
+    splitter=RandomSplitter(valid_pct=0.2, seed=42),
+    get_y=parent_label,
+    item_tfms=[Resize(192, method='squish')]
+).dataloaders(path, bs=32)
+
+dls.show_batch(max_n=6)
+
+# %% [markdown]
+# Here what each of the `DataBlock` parameters means:
+# 
+#     blocks=(ImageBlock, CategoryBlock),
+# 
+# The inputs to our model are images, and the outputs are categories (in this case, "bird" or "forest").
+# 
+#     get_items=get_image_files, 
+# 
+# To find all the inputs to our model, run the `get_image_files` function (which returns a list of all image files in a path).
+# 
+#     splitter=RandomSplitter(valid_pct=0.2, seed=42),
+# 
+# Split the data into training and validation sets randomly, using 20% of the data for the validation set.
+# 
+#     get_y=parent_label,
+# 
+# The labels (`y` values) is the name of the `parent` of each file (i.e. the name of the folder they're in, which will be *bird* or *forest*).
+# 
+#     item_tfms=[Resize(192, method='squish')]
+# 
+# Before training, resize each image to 192x192 pixels by "squishing" it (as opposed to cropping it).
+
+# %% [markdown]
+# Now we're ready to train our model. The fastest widely used computer vision model is `resnet18`. You can train this in a few minutes, even on a CPU! (On a GPU, it generally takes under 10 seconds...)
+# 
+# `fastai` comes with a helpful `fine_tune()` method which automatically uses best practices for fine tuning a pre-trained model, so we'll use that.
+
+# %%
+learn = vision_learner(dls, resnet18, metrics=error_rate)
+learn.fine_tune(3)
+
+# %% [markdown]
+# Generally when I run this I see 100% accuracy on the validation set (although it might vary a bit from run to run).
+# 
+# "Fine-tuning" a model means that we're starting with a model someone else has trained using some other dataset (called the *pretrained model*), and adjusting the weights a little bit so that the model learns to recognise your particular dataset. In this case, the pretrained model was trained to recognise photos in *imagenet*, and widely-used computer vision dataset with images covering 1000 categories) For details on fine-tuning and why it's important, check out the [free fast.ai course](https://course.fast.ai/).
+
+# %% [markdown]
+# ## Step 3: Use our model (and build your own!)
+
+# %% [markdown]
+# Let's see what our model thinks about that bird we downloaded at the start:
+
+# %%
+is_bird,_,probs = learn.predict(PILImage.create('bird.jpg'))
+print(f"This is a: {is_bird}.")
+print(f"Probability it's a bird: {probs[0]:.4f}")
+
+# %% [markdown]
+# Good job, resnet18. :)
+# 
+# So, as you see, in the space of a few years, creating computer vision classification models has gone from "so hard it's a joke" to "trivially easy and free"!
+# 
+# It's not just in computer vision. Thanks to deep learning, computers can now do many things which seemed impossible just a few years ago, including [creating amazing artworks](https://openai.com/dall-e-2/), and [explaining jokes](https://www.datanami.com/2022/04/22/googles-massive-new-language-model-can-explain-jokes/). It's moving so fast that even experts in the field have trouble predicting how it's going to impact society in the coming years.
+# 
+# One thing is clear -- it's important that we all do our best to understand this technology, because otherwise we'll get left behind!
+
+# %% [markdown]
+# Now it's your turn. Click "Copy & Edit" and try creating your own image classifier using your own image searches!
+# 
+# If you enjoyed this, please consider clicking the "upvote" button in the top-right -- it's very encouraging to us notebook authors to know when people appreciate our work.
+
+# %%
+
+
+
